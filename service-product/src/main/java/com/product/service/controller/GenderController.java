0190c339739ec4198ce031c92a080b1cb1ba9b09
gabrielseiko <gabriel-s98@hotmail.com>
2024-10-19 23:58:37 -0500

    Controller de Product y gender

--- service-product/src/main/java/com/product/service/controller/GenderController.java
+++ service-product/src/main/java/com/product/service/controller/GenderController.java
@@ -1,4 +1,89 @@
 package com.product.service.controller;
 
+import com.product.service.entity.Gender;
+import com.product.service.service.GenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/url/gender")
 public class GenderController {
+
+    @Autowired
+    private GenderService service;
+
+    //LISTADO
+    @GetMapping
+    public ResponseEntity<List<Gender>> listGender(){
+        List<Gender> respose = service.list();
+        return ResponseEntity.ok(respose);
+    }
+    //BUSCAR
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> searchGender(@PathVariable("id") int idGender){
+        Map<String, Object> response = new HashMap<>();
+        Optional<Gender> optional = service.search(idGender);
+
+        if (optional.isPresent()){
+            response.put("gender", optional.get());
+        } else {
+            response.put("message", "Genero no encontrado");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //REGISTRAR
+    @PostMapping("/register")
+    @ResponseBody
+    public ResponseEntity<?> registerGender(@RequestBody Gender objGender){
+        Map<String, Object> response = new HashMap<>();
+        Gender  objResponse = service.register(objGender);
+
+        if(objResponse != null){
+            response.put("message", "Genero registrado");
+            response.put("Gender", objResponse);
+        } else {
+            response.put("message", "Error no se registro");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //ACTUALIZAR
+    @PutMapping("/update")
+    @ResponseBody
+    public ResponseEntity<?> updateGender(@RequestBody Gender objGender){
+        Map<String, Object> response = new HashMap<>();
+        Gender objResponse = service.register(objGender);
+
+        if (objResponse != null){
+            response.put("message", "Genero actualizado");
+            response.put("Gender", objResponse);
+        } else {
+            response.put("message", "Error no se actualizo");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //ELIMINAR
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> deleteGender(@PathVariable("id") int idGender){
+        Map<String, Object> response = new HashMap<>();
+
+        //verificar si existe el genero
+        Optional<Gender> optional = service.search(idGender);
+
+        if (optional.isPresent()){
+            service.delete(idGender);
+            response.put("message", "Genero eliminado");
+        } else {
+            response.put("message", "Error no se elimino");
+        }
+        return ResponseEntity.ok(response);
+    }
 }
--- service-product/src/main/java/com/product/service/controller/ProductController.java
+++ service-product/src/main/java/com/product/service/controller/ProductController.java
@@ -1,4 +1,87 @@
 package com.product.service.controller;
 
+import com.product.service.entity.Product;
+import com.product.service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/url/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    //LISTADO
+    @GetMapping
+    public ResponseEntity<List<Product>> listProduct(){
+        List<Product> response = service.list();
+        return ResponseEntity.ok(response);
+    }
+    //BUSCAR
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> searchProduct(@PathVariable("id") int idProduct){
+        Map<String, Object> response = new HashMap<>();
+        Optional<Product> optional = service.search(idProduct);
+
+        if (optional.isPresent()){
+            response.put("product", optional.get());
+        } else {
+            response.put("message", "Producto no encontrado");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //REGISTRAR
+    @PostMapping("/register")
+    @ResponseBody
+    public ResponseEntity<?> registerProduct(@RequestBody Product obj){
+        Map<String, Object> response = new HashMap<>();
+        Product objResponse = service.register(obj);
+
+        if (objResponse != null){
+            response.put("message", "Producto registrado");
+            response.put("product", obj);
+        } else {
+            response.put("message", "Error no se registro");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //ACTUALIZAR
+    @PutMapping("/update")
+    @ResponseBody
+    public ResponseEntity<?> updateProduct(@RequestBody Product obj){
+        Map<String, Object> response = new HashMap<>();
+        Product objResponse = service.register(obj);
+
+        if(objResponse != null) {
+            response.put("message", "Producto Actualizado");
+            response.put("Producto", obj);
+        } else {
+            response.put("message", "Error no se actualizo");
+        }
+        return ResponseEntity.ok(response);
+    }
+    //ELIMINAR
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> deleteProduct(@PathVariable("id") int idProduct){
+        Map<String, Object> response = new HashMap<>();
+        //verificar si existe
+        Optional<Product> optional = service.search(idProduct);
+
+        if (optional.isPresent()){
+            service.delete(idProduct);
+            response.put("message", "Producto eliminado");
+        }else {
+            response.put("message", "Error");
+        }
+        return ResponseEntity.ok(response);
+    }
 }
